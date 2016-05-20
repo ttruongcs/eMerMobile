cff08fde78fa1ed3e906757ba1d783aa34abf790
tantruong89 <12345678@x>
2016-05-20 13:00:53 +0700

    revise outlet

--- app/src/main/java/com/banvien/fcv/mobile/BaseActivity.java
+++ app/src/main/java/com/banvien/fcv/mobile/BaseActivity.java
@@ -1,6 +1,5 @@
 package com.banvien.fcv.mobile;
 
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
